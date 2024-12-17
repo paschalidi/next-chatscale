5c9c70662d6eff2f94613e12ea0f4cb6f8e409c2
Christos Paschalidis <paschalidi@outlook.com>
2024-12-17 20:45:53 +0000

    fixes

--- apps/web/app/docs/page.tsx
+++ apps/web/app/docs/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Header } from "@/components/layout/header";
 import { DocsSidebar } from "@/components/docs/sidebar";
 import { DocsContent } from "@/components/docs/content";
 
